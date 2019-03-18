96e89b69d0c0f291dfdbf77d10ea981113342529
Sam Cronin <croninsw@gmail.com>
2019-03-18 17:15:05 -0500

    Created beginnings of search

--- src/Components/Plots/PlotSearch.js
+++ src/Components/Plots/PlotSearch.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react"
 import PlotCard from "./PlotCard"
+import Search from "../Search/Search";
 
 export default class PlotSearch extends Component {
     render () {
         return (
             <React.Fragment>
+                <Search />
+
                 <section className="plots">
                 {
                     this.props.plots.map(plot =>
