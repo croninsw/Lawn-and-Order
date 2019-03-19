023bcbdae74fa5f96f59df5d80aac931c219967f
Sam Cronin <croninsw@gmail.com>
2019-03-19 15:37:54 -0500

    Removed old files

--- src/Components/Plots/PlotSearch.js
+++ src/Components/Plots/PlotSearch.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react"
 import PlotCard from "./PlotCard"
-import Search from "../Search/Search";
 
 export default class PlotSearch extends Component {
     render () {
         return (
             <React.Fragment>
-                <Search />
 
                 <section className="plots">
                 {
